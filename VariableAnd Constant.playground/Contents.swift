013b2bb026c01ca99e0a8e9a4d6e95ea51593293
meaw <keaitisak@gmail.com>
2018-10-06 14:29:58 +0700

    i undertand let

--- VariableAnd Constant.playground/Contents.swift
+++ VariableAnd Constant.playground/Contents.swift
@@ -8,6 +8,23 @@ var numnerInt = 123 // integer type
 var numberDou  = 12.34  // Double Type
 var statusBool = true   // boolean type
 
+//Try to Change Value
+//var can edit value
+nameString = "Doramon"
+numnerInt = 555
+numberDou = 14.56
+statusBool = !statusBool
+
+//This is constant
+let surNameString = "Keaitisak"
+//surNameString = "AAAAA" can't Edit value in let type
+let number1Int = 444
+let number1Dou = 12.45
+let status1Bool = true
+
+
+
+
 
 
 

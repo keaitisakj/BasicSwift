492da651c0c1df3bfda4c44a62a1999c7d0aa611
meaw <keaitisak@gmail.com>
2018-10-06 14:19:59 +0700

    Var

---
+++ VariableAnd Constant.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//การประกาศตัวแปร
+
+//นี่คือการประกาศตัวแปร แบบ Implicit  หรือ การปรกาศแบบ ให้  value ในการกำหนด data type
+var nameString = "Meaw" // String type
+var numnerInt = 123 // integer type
+var numberDou  = 12.34  // Double Type
+var statusBool = true   // boolean type
+
+
+
+

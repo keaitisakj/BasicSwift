48c7259497e023e972cd2ba048d50d4483bf68b0
meaw <keaitisak@gmail.com>
2018-10-06 14:44:22 +0700

    This is Operating

--- VariableAnd Constant.playground/Contents.swift
+++ VariableAnd Constant.playground/Contents.swift
@@ -8,6 +8,13 @@ var numnerInt = 123 // integer type
 var numberDou  = 12.34  // Double Type
 var statusBool = true   // boolean type
 
+//Explicit
+var number2String: String = "Nobita"
+var number2Dou: Double = 22.44
+var stattus2Bool: Bool = true
+
+
+
 //Try to Change Value
 //var can edit value
 nameString = "Doramon"
@@ -21,6 +28,16 @@ let surNameString = "Keaitisak"
 let number1Int = 444
 let number1Dou = 12.45
 let status1Bool = true
+
+//Explicitlet
+let  numbereInt: Int = 55
+
+//operrating
+let answer1Int = numbereInt + Int(numberDou) // Change double to int
+let anser1Dou: Double = numberDou + Double(number1Dou) // Int to double
+let answer1Sting: String = nameString + surNameString + String(number1Int)
+
+
 
 
 
